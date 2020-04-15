d126b519f3bbf1c39510d22359f52804ed451f9f
rafaelvalentine <ozichukwuezike@hotmail.com>
2020-04-15 18:15:25 +0100

    added test for get route

--- server/test/index.test.js
+++ server/test/index.test.js
@@ -1,21 +1,25 @@
 const request = require('supertest')
 const expect = require('expect')
-const express = require('express')
-
 
 const { app } = require('../index')
 const { Todo } = require('../database')
-
+const testTodos = [
+    { text: 'todod 1' },
+    { text: 'todo 2' }
+]
 beforeEach(done => {
     Todo.remove({})
-        .then(() => done())
+        // .then(() => done())
+        .then(() => {
+            Todo.insertMany(testTodos)
+                .then(() => done())
+        })
 })
-describe('POST /todo', () => {
-
+describe('POST /api/v1/todo', () => {
     it('should create a new Todo', done => {
         const text = 'Test create Todo'
         request(app)
-            .post('/todo')
+            .post('/api/v1/todo')
             .send({ text })
             .expect(200)
             .expect(res => {
@@ -30,26 +34,38 @@ describe('POST /todo', () => {
                         done()
                     }).catch((err) => {
                         done(err)
-                    });
+                    })
             })
     })
 
-
     it('should not create a new Todo', done => {
         request(app)
-            .post('/todo')
+            .post('/api/v1/todo')
             .send({})
             .expect(400)
             .end((err, res) => {
-
                 if (err) return done(err)
                 Todo.find()
                     .then((result) => {
-                        expect(result.length).toBe(0)
+                        expect(result.length).toBe(2)
                         done()
                     }).catch((err) => {
                         done(err)
-                    });
+                    })
             })
+    })
+})
+
+
+
+describe('GET /api/v1/todo', () => {
+    it('should get all todos', done => {
+        request(app)
+            .get('/api/v1/todo')
+            .expect(200)
+            .expect(res => {
+                expect(res.body.data.length).toBe(2)
+            })
+            .end(done)
     })
 })
